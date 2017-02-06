0762223ee268c24581c43f969e86e8c54d56f58e
epenaranda007 <eugenepenaranda007@gmail.com>
2017-02-06 15:32:39 -0800

    Bare Requirements completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node) {
   // your code here
+  var nodes = node || document.body;
+  var result = [];
+
+  //base case
+  if(Array.from(nodes.classList).indexOf(className) >= 0){
+    result.push(nodes);
+  }
+
+  //recursive
+  if(nodes.childNodes.length > 0 ){
+    for(var i = 0; i < nodes.childNodes.length; i++){
+      if(nodes.childNodes[i].nodeType === 1){
+        result = result.concat(getElementsByClassName(className, nodes.childNodes[i]));
+      }
+    }
+  }
+
+  return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,32 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // Array
+  if(Array.isArray(obj)){
+    var tempArray = [];
+    for(var i = 0; i < obj.length; i++){
+      // try _.map
+      tempArray.push(stringifyJSON(obj[i]));
+    }
+    return '[' + tempArray.join(',') + ']';
+  }
+  // Object
+  if(obj && typeof obj === 'object'){
+    var tempArray = [];
+    for(var key in obj){
+      // Functions and undefined cases
+      if(typeof obj[key] === 'function' || typeof obj[key] === 'undefined'){
+        continue;
+      }
+      tempArray.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key])); 
+    }
+    return '{' + tempArray.join(',') + '}'; 
+  }
+  // String
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  // Default
+  return ''+obj;
+
 };
